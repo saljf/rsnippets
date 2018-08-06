2db15cc6773e9adfb57158e24425c5e2b6e49bd3
saljf <31564784+saljf@users.noreply.github.com>
2018-08-06 15:55:22 +1000

    Update import-data.R

--- import-data.R
+++ import-data.R
@@ -4,6 +4,19 @@
 
 # Read in CSV
 df <- read_csv('data.csv')
+df <- read.csv('data.csv', stringsAsFactors = FALSE)
+
+# Rename columns
+colnames(df) <- c('col1', 'col2', etc)
+
+# Replace NAs with 0
+df$col[is.na(df$col)] <- 0
+
+# Replace blanks with NA
+df[df == ''] <- NA
+
+# Fill values forward to replace following NAs (zoo package)
+df$col <- na.locf(df$col)
 
 # Convert to factor
 df$col <- as.factor(df$col)
@@ -39,8 +52,14 @@ df$col <- as.integer(format(df$col, '%H'))
 # Convert to title case (stringr package)
 df$col <- str_to_title(df$col)
 
+# Find and replace
+df$col <- gsub('existingstring', 'replacementstring', df$col)
+
 # Find and replace (stringr package)
 df$col <- str_replace_all(df$col, 'existingstring', 'replacementstring')
+
+# Find and replace, conditional on existing variable
+df$col <- ifelse(col == condition, value1, value2)
 
 # Create new variable, value conditional on existing variable
 df <- df %>%
